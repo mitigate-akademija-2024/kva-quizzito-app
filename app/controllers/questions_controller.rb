ed1165e38af4490b61d24c904e2b7f1c0c441f3b
Kristaps V <95536069+Kastrups@users.noreply.github.com>
2024-08-09 09:36:30 +0000

    Quiz answer/question table

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,14 @@ class QuestionsController < ApplicationController
 
   def create
     @question = @quiz.questions.new(question_params)
+
+    @question.answers.new answer_text: params[:question][:answer1]
+    @question.answers.new answer_text: params[:question][:answer2]
+    @question.answers.new answer_text: params[:question][:answer3]
+
+
+
+
     if @question.save
       flash.notice = "Question was successfully created."
       redirect_to quiz_url(@quiz)
@@ -16,6 +24,17 @@ class QuestionsController < ApplicationController
 
   def new
     @question = @quiz.questions.new
+    5.times { @question.answers.build } # Prepares 3 answer fields
+  end
+
+  def create
+    @question = @quiz.questions.new(question_params)
+    if @question.save
+      flash.notice = "Jautājums veiksmīgi izveidots"
+      redirect_to quiz_url(@quiz)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -25,6 +44,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:question_text)
+    params.require(:question).permit(:question_text, answers_attributes: [:id, :answer_text, :correct, :_destroy])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,9 @@ class Question < ApplicationRecord
   
     belongs_to :quiz
     has_many :answers
+
+    accepts_nested_attributes_for :answers, allow_destroy: true
+
+    validates :question_text, presence: true
+    validates_associated :answers
   end
